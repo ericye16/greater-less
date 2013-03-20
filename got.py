fb3648c2f6e1f06a5840e865cd541078edc78fd7
Eric Ye <ericye16@gmail.com>
2013-03-20 12:52:17 -0400

    Added posting...

--- got.py
+++ got.py
@@ -1,8 +1,16 @@
-import webapp2
+import webapp2, jinja2, os
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write("Hello world.")
+        indexTemplate = jinja_environment.get_template('index.html')
+        self.response.out.write(indexTemplate.render())
+
+    def post(self):
+        number = self.request.get('number')
+        
 
 app = webapp2.WSGIApplication([('/', MainPage),
                                ],
