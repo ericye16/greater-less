282127e007e4a8764b33727e00727fe3806f783d
Eric Ye <ericye16@gmail.com>
2013-03-21 02:07:35 -0400

    Changed html, got the crypto library, added to script

--- got.py
+++ got.py
@@ -1,4 +1,5 @@
 import webapp2, jinja2, os
+from Crypto.Random import random
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
@@ -10,8 +11,15 @@ class MainPage(webapp2.RequestHandler):
 
     def post(self):
         number = self.request.get('number')
-        
-
+        try:
+            number = float(number)
+        except:
+            self.response.out.write("You should enter an actual number.\n"
+                                    "Any decimal number is acceptable.\n"
+                                    '<a href="/">Return.</a>')
+            return
+        #Generate two random numbers, check if they're already there,
+        #commit it into the datastore and give the guy links
 app = webapp2.WSGIApplication([('/', MainPage),
                                ],
                                debug = True,
